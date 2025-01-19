187868e0a2611c5760fac74d75e823427eb75441
KaziMithfa <kazimithfa99@gmail.com>
2025-01-19 23:44:18 +0100

    security issue solved

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCR50dLTyKMZKw6H7AIpk_QGiX85Vxk85c",
-  authDomain: "real-estate-35332.firebaseapp.com",
-  projectId: "real-estate-35332",
-  storageBucket: "real-estate-35332.firebasestorage.app",
-  messagingSenderId: "169701342844",
-  appId: "1:169701342844:web:53a04c261d90a7d06513f9",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
